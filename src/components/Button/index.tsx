88ca1915decfbfb746af1476326c43a23d54812b
gstvds <gstvds@icloud.com>
2024-04-18 22:45:48 -0300

    fix: change button icon to be optional
    Signed-off-by: gstvds <gstvds@icloud.com>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ import { useTheme } from 'styled-components/native';
 import { VStack, HStack, Label } from './styles';
 
 interface ButtonProps extends TouchableOpacityProps {
-  icon: React.FC<IconProps>;
+  icon?: React.FC<IconProps>;
   solid?: boolean;
   label: string;
 }
@@ -16,7 +16,7 @@ export function Button({ icon: Icon, solid, label, ...rest }: ButtonProps) {
   return (
     <VStack solid={solid} {...rest}>
       <HStack>
-        <Icon size={18} color={solid ? theme.colors.white : theme.colors.gray_100} />
+        {Icon && <Icon size={18} color={solid ? theme.colors.white : theme.colors.gray_100} />}
         <Label solid={solid}>{label}</Label>
       </HStack>
     </VStack>
